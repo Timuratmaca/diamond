27caa956189a051f8c6482bfa88b403c4a2492e4
Timuratmaca <115021588+Timuratmaca@users.noreply.github.com>
2024-04-22 22:53:27 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let collections=document.getElementsByClassName("collection")
+let images=document.getElementsByClassName("products")[0].getElementsByTagName("img")
+
+for(let a of images){
+    a.style.height=a.offsetWidth+"px"
+}
 
 for(let c of collections){
     c.style.height=c.offsetWidth+"px"
